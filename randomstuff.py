26f442fa830fb4d9ac6155ad64a4ce368ea219bd
Fighterfax <dentonwhiteside@amdg.rockhursths.edu>
2020-02-07 08:44:35 -0600

    Random

---
+++ randomstuff.py
@@ -0,0 +1,9 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+hello this is random stuff
+SyntaxError: invalid syntax
+>>> print("hello how are you")
+hello how are you
+>>> 
